4446778dc0f3ea0383a193946a6c63c222cba338
Mscode04 <ms.shaheenkp@gmail.com>
2024-03-12 10:23:08 +0530

    new

---
+++ MT.py
@@ -0,0 +1,10 @@
+from tkinter import*
+window=Tk()
+
+
+window.title("TECH HUB")
+window.geometry("600x400")
+window.resizable(False,False)
+window.iconbitmap("C:\Users\Administrator\Downloads\HAC.ico")
+
+window.mainloop()
